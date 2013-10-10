7264882eee3eeada7489311b72a75a059ec51965
Alice-MacAir <amchung@stanford.edu>
2013-10-10 13:57:05 -0700

    blinking

--- euglena/js/joystick.js
+++ euglena/js/joystick.js
@@ -161,12 +161,12 @@ function onReady(event) {
 		for (var i=0;i<score.length;i++){
 			board.append(score[i][0]+'  :  '+score[i][1]+ '<br />');
 		}
-		board.fadeOut('slow');
-		board.fadeIn('slow');
-		board.fadeOut('slow');
-		board.fadeIn('slow');
-		board.fadeOut('slow');
-		board.fadeIn('slow');
+		board.fadeOut('fast');
+		board.fadeIn('fast');
+		board.fadeOut('fast');
+		board.fadeIn('fast');
+		board.fadeOut('fast');
+		board.fadeIn('fast');
 	});
 
 	socket.on('disconnect', function() {
